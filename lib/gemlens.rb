4d64a70cb513624200e1fe83e602fcf2e9da07dd
Fai Wong <wongwf82@gmail.com>
2025-07-15 13:32:38 +0800

    Add gemlens executable

---
+++ lib/gemlens.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gemlens/version"
+
+module Gemlens
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gemlens/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Gemlens
+  VERSION = "0.1.0"
+end
